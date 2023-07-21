913cbcfb91fcec149d635698b3a582d06208b0c8
milad <100165608+milord12345@users.noreply.github.com>
2023-07-22 02:01:43 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xd1055fb9d24a6a82ba43b7b556b1238362a9950a";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
